8191b967947e688dc4a044ffcacadfce1d34215a
Raju9934 <raju.gupta10u@gmail.com>
2024-10-27 19:24:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>res.json({message:'Hey Everyone from AWS Server '}));
-app.listen(process.env.PORT || 8000,()=>console.log('Server Started'));
+app.listen(process.env.PORT || 8003,()=>console.log('Server Started'));
